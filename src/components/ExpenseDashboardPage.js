6382f3613c4a814f217db74988512b9d395482df
AndrewS <mail@gmail.com>
2018-02-04 21:28:27 +0200

    Setup dev

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilers from './ExpenseListFilters';
+import ExpenseSummary from "./ExpenseSummary";
 
 const ExpenseDashboardPage = () => (
   <div>
     <ExpenseListFilers />
     <ExpenseList />
+    <ExpenseSummary />
   </div>
 );
 

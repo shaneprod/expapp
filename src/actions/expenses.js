c56b4b96d22a989f7e4b4c1e97bff30bac2276a7
AndrewS <mail@gmail.com>
2018-02-05 18:19:16 +0200

    With firebase set update dev

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -32,6 +32,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  };
+};
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
@@ -39,27 +47,33 @@ export const editExpense = (id, updates) => ({
   updates
 });
 
+export const startEditExpense = (id, updates) => {
+  return (dispatch) => {
+    return database.ref(`expenses/${id}`).update(updates).then(() => {
+      dispatch(editExpense(id, updates));
+    });
+  };
+};
 
+// SET_EXPENSES
 export const setExpenses = (expenses) => ({
   type: 'SET_EXPENSES',
   expenses
 });
 
-
 export const startSetExpenses = () => {
   return (dispatch) => {
-
     return database.ref('expenses').once('value').then((snapshot) => {
-      const allExpenses = [];
-     
+      const expenses = [];
+
       snapshot.forEach((childSnapshot) => {
-        allExpenses.push({
-          id:childSnapshot.key,
+        expenses.push({
+          id: childSnapshot.key,
           ...childSnapshot.val()
-        })
+        });
       });
-      
-      dispatch(setExpenses(allExpenses));
+
+      dispatch(setExpenses(expenses));
     });
   };
 };
